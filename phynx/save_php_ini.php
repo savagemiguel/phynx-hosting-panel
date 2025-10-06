56995dc023218e0b113721601bfaf27e6ddfabb8
Miguel Savage <130864213+savagemiguel@users.noreply.github.com>
2025-10-05 23:06:03 -0400

    Delete phynx directory

--- phynx/save_php_ini.php
+++
@@ -1,9 +0,0 @@
-<?php
-$ini_path = php_ini_loaded_file();
-
-if ($_POST['ini_content'] && $ini_path && is_writable($ini_path)) {
-    $result = file_put_contents($ini_path, $_POST['ini_content']);
-    echo $result !== false ? 'success' : 'error: write failed.';
-} else {
-    echo 'Error: File not writable or no ini file is found.';
-}
